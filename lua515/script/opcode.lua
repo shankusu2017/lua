5cc22d77e68207f5cc08603f9f8832056d5718b5
shankusu2017 <shankusu2017@gmail.com>
2021-04-23 23:33:56 +0800

    lbaselib.c阅读完毕

--- lua515/script/opcode.lua
+++ lua515/script/opcode.lua
@@ -1,19 +1,24 @@
-local a = 0
-local b = 1
-local c = a + b
+local t1 = {}
+local t2 = {}
+setmetatable(t1, t2)
+print("type(t1.getmetatable())", type(getmetatable(t1)))
+-- local a = tonumber("3")
+-- local a = 0
+-- local b = 1
+-- local c = a + b
 
-local ret1, ret2 = math.random(100)
+-- local ret1, ret2 = math.random(100)
 
-print("hello lua")
-local tbl = {n1 = 1, n2 = 2, n3 =3,}
-for _ in pairs(tbl) do
-    print("i'm loop tbl")
-end
+-- print("hello lua")
+-- local tbl = {n1 = 1, n2 = 2, n3 =3,}
+-- for _ in pairs(tbl) do
+--     print("i'm loop tbl")
+-- end
 
-for n in pairs(_G) do
-    print(n)
-end
+-- for n in pairs(_G) do
+--     print(n)
+-- end
 
-a = 1
-setfenv(1, {})
+-- a = 1
+-- setfenv(1, {})
 
